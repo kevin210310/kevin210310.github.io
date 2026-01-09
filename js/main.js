29e5d8dd2c386abc68afc7de1caf7c30051254ba
DevsSnapp <kevinabarca1197@gmail.com>
2026-01-09 04:21:31 -0300

    Maqueta final

--- js/main.js
+++ js/main.js
@@ -39,3 +39,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
     initCarousel();
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetSection = document.querySelector(targetId);
+
+        targetSection.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
